fb41333f386d5ae8ddadc8f2b8edd97c8dd0b14d
Farahani Samat <farahanis@gmail.com>
2019-06-08 23:51:01 +1000

    Refactor code and add pagination

--- data/ticket_list.py
+++ data/ticket_list.py
@@ -1,15 +1,12 @@
 import requests
 import json
-import pandas as pd
 
-response = requests.get('https://{subdomain}.zendesk.com/api/v2/tickets.json', auth=({username}, {password}))
-response_text = response.text #Convert 'Response' to text.
+url = 'https://{subdomain}.zendesk.com/api/v2/tickets.json?per_page=25'
 
-list_data_dict = json.loads(response_text)
-list_data = list_data_dict['tickets']
-
-df = pd.DataFrame(list_data) #Convert dict into dataframe
-df.reset_index(level=0, inplace=True)
-
-to_display = df[['id', 'subject', 'status', 'priority']]
-print(to_display)
+while url:
+    response = requests.get(url, auth=('{username}', '{password}'))
+    data = response.json()
+    for item in data['tickets']:
+        keys = ['id', 'subject', 'status', 'priority']
+        print ([item.get(key) for key in keys])
+    url = data['next_page']
