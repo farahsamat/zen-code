d06f0697a9b220f734444974ea2b1a9ae8281e8a
Farahani Samat <farahanis@gmail.com>
2019-06-10 20:34:01 +1000

    - Create 'Tickets' class which contains 'view_list' and 'view_ticket' methods - Use environment variables for authentication credentials

--- data/ticket_list.py
+++ data/ticket_list.py
@@ -1,24 +1,34 @@
 import requests
 import json
 import pandas as pd
+import os
+from dotenv import load_dotenv
 
-#pagination
-pagination = '?per_page=25'
-per_page = 'https://{}.zendesk.com/api/v2/tickets.json{}'.format(subdomain, pagination)
-while per_page:
-    r = requests.get(per_page, auth=auth)
-    data = r.json()
-    for ticket in data['tickets']:
-        keys = ['id', 'subject', 'status', 'priority']
-        print ([ticket.get(key) for key in keys])
-    per_page = data['next_page']
+load_dotenv()
 
-#view ticket
-ticket_id = input("Please enter a ticket number: ")
-ticket_url = 'https://{}.zendesk.com/api/v2/tickets/{}.json'.format(subdomain, ticket_id)
-try:
-    r = requests.get(ticket_url, auth=auth)
-    data = r.json()
-    print (pd.DataFrame(data))
-except ValueError:
-    print("Invalid ticket number")
+subdomain = os.getenv("SUBDOMAIN")
+username = os.getenv("USERNAME")
+token_key = os.getenv("TOKEN")
+per_page = str(25)
+data_to_display = ['id', 'subject', 'status', 'priority']
+
+
+class Tickets:
+    def view_list(self):
+        pagination = 'https://{}.zendesk.com/api/v2/tickets.json?per_page={}'.format(subdomain, per_page)
+        while pagination:
+            r = requests.get(pagination, auth=(username, token_key))
+            data = r.json()
+            for ticket in data['tickets']:
+                print ([ticket.get(key) for key in data_to_display])
+            pagination = data['next_page']
+
+    def view_ticket(self):
+        ticket_id = input("Please enter a ticket number: ")
+        ticket_url = 'https://{}.zendesk.com/api/v2/tickets/{}.json'.format(subdomain, ticket_id)
+        try:
+            r = requests.get(ticket_url, auth=(username, token_key))
+            data = r.json()
+            print (pd.DataFrame(data))
+        except ValueError:
+            print("Invalid ticket number")
