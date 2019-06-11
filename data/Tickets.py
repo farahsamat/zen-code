18ea95a89552556bcf1d79a0cf98d36b71db1fb5
Farahani Samat <farahanis@gmail.com>
2019-06-11 22:01:48 +1000

    Change authentication variables

--- data/Tickets.py
+++ data/Tickets.py
@@ -2,33 +2,30 @@ import requests
 import json
 import pandas as pd
 import os
-from tabulate import tabulate
 from beautifultable import BeautifulTable
 from dotenv import load_dotenv
 
 load_dotenv()
-
-subdomain = os.getenv("SUBDOMAIN")
 username = os.getenv("USERNAME")
-token_key = os.getenv("TOKEN")
-per_page = str(25)
-data_to_display = ['id', 'subject']
+password = os.getenv("PASSWORD")
+
+data_to_display = ['id', 'subject', 'status']
 
 class Tickets:
     def view_list(self):
-        start_count = 0
+        page_count = 0
         table = BeautifulTable()
-        table.column_headers = ['id', 'subject']
-        pagination = 'https://{}.zendesk.com/api/v2/tickets.json?per_page={}'.format(subdomain, per_page)
+        table.column_headers = data_to_display
+        pagination = 'https://farahsamat.zendesk.com/api/v2/tickets.json?per_page=25'
         while pagination:
-            r = requests.get(pagination, auth=(username, token_key))
+            r = requests.get(pagination, auth=(username, password))
             data = r.json()
             for ticket in data['tickets']:
                 table.append_row([ticket.get(key) for key in data_to_display])
             print (table)
-            start_count += 1
+            page_count += 1
             pagination = data['next_page']
-            table.append_row(['Page','<     {}     >'.format(str(start_count))])
+            table.append_row(['***','<     End of page {}     >'.format(str(start_count)),'***'])
             next_page = input("Press 'enter' to continue ")
             if next_page.lower() == ' ':
                 continue
@@ -36,9 +33,9 @@ class Tickets:
     def view_ticket(self):
         try:
             ticket_id = input("Please enter a ticket number: ")
-            ticket_url = 'https://{}.zendesk.com/api/v2/tickets/{}.json'.format(subdomain, ticket_id)
-            r = requests.get(ticket_url, auth=(username, token_key))
+            ticket_url = 'https://farahsamat.zendesk.com/api/v2/tickets/{}.json'.format(ticket_id)
+            r = requests.get(ticket_url, auth=(username, password))
             data = r.json()
-            print (pd.DataFrame(data)[['submitter_id', 'created at', 'subject', 'description',]])
+            print (pd.DataFrame(data))
         except ValueError:
-            print("Invalid ticket number")
+            print("**Invalid ticket number**")
