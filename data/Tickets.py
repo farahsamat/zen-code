cc202aa2b600234a5bd41126fc1a6d255e033256
Farahani Samat <farahanis@gmail.com>
2019-06-12 08:38:29 +1000

    Display paginated data in separate tables for better viewing

--- data/Tickets.py
+++ data/Tickets.py
@@ -13,19 +13,19 @@ data_to_display = ['id', 'subject', 'status']
 
 class Tickets:
     def view_list(self):
-        page_count = 0
-        table = BeautifulTable()
-        table.column_headers = data_to_display
+        page_count = 1
         pagination = 'https://farahsamat.zendesk.com/api/v2/tickets.json?per_page=25'
         while pagination:
+            table = BeautifulTable()
+            table.column_headers = data_to_display
             r = requests.get(pagination, auth=(username, password))
             data = r.json()
             for ticket in data['tickets']:
                 table.append_row([ticket.get(key) for key in data_to_display])
+            table.append_row(['','<<<     End of page {}     >>>'.format(str(page_count)),''])
             print (table)
             page_count += 1
             pagination = data['next_page']
-            table.append_row(['***','<     End of page {}     >'.format(str(start_count)),'***'])
             next_page = input("Press 'enter' to continue ")
             if next_page.lower() == ' ':
                 continue
