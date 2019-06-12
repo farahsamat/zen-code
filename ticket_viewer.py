24b85f4128aece0aa0988f0c4160c9c34300311f
Farahani Samat <farahanis@gmail.com>
2019-06-12 14:13:19 +1000

    Bring files to root directory

---
+++ ticket_viewer.py
@@ -0,0 +1,40 @@
+from tickets import Tickets
+import numpy as np
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+
+ticket_list = Tickets()
+select_ticket = Tickets()
+
+main_menu = np.array(["Display ticket list", "View ticket", "Quit"])
+
+def input_number(prompt):
+    while True:
+        try:
+            num=float(input(prompt))
+            break
+        except ValueError:
+            print(" ")
+            pass
+    return num
+
+def display_menu(options):
+    print(" ")
+    print ("----------MAIN MENU-----------------")
+    for i in range(len(options)):
+        print("{:d}. {:s}".format(i+1, options[i]))
+
+    choice = 0
+    while not(np.any(choice == np.arange(len(options))+1)):
+        choice = input_number("Please choose your option: ")
+        print(" ")
+    return choice
+
+while True:
+    choice = display_menu(main_menu)
+    if choice == 1:
+        ticket_list.view_list()
+    elif choice == 2:
+        select_ticket.view_ticket()
+    elif choice == 3:
+        break
---
+++ tickets.py
@@ -0,0 +1,64 @@
+import requests
+import json
+import pandas as pd
+import os
+from beautifultable import BeautifulTable
+from dotenv import load_dotenv
+
+load_dotenv()
+username = os.getenv("USERNAME")
+password = os.getenv("PASSWORD")
+url = 'https://farahsamat.zendesk.com/api/v2/tickets'
+per_page = '?per_page=25'
+data_to_display = ['id', 'subject', 'status']
+
+class Tickets:
+    def view_list(self):
+        page_count = 1
+        pagination = '{}.json{}'.format(url, per_page)
+        try:
+            while pagination:
+                table = BeautifulTable()
+                table.column_headers = data_to_display
+                r = requests.get(pagination, auth=(username, password))
+                data = r.json()
+                for ticket in data['tickets']:
+                    table.append_row([ticket.get(key) for key in data_to_display])
+                table.append_row(['','<<<     End of page {}     >>>'.format(str(page_count)),''])
+                print (table)
+                page_count += 1
+                pagination = data['next_page']
+                next_page = input("Press 'enter' to continue ")
+                if next_page.lower() == ' ':
+                    continue
+        except KeyError:
+            print("URL/Authentication error")
+        except requests.exceptions.RequestException as err:
+            print ("Request Error ",err)
+        except requests.exceptions.HTTPError as errh:
+            print ("Http Error: ",errh)
+        except requests.exceptions.ConnectionError as errc:
+            print ("Error Connecting: ",errc)
+        except requests.exceptions.Timeout as errt:
+            print ("Timeout Error: ",errt)
+
+    def view_ticket(self):
+        try:
+            while url:
+                ticket_id = input("Please enter a ticket number: ")
+                ticket_url = '{}/{}.json'.format(url, ticket_id)
+                r = requests.get(ticket_url, auth=(username, password))
+                data = r.json()
+                print (pd.DataFrame(data))
+        except KeyError:
+            print("URL/Authentication error")
+        except ValueError:
+            print("Invalid ticket/Authentication error")
+        except requests.exceptions.RequestException as err:
+            print ("Request Error ",err)
+        except requests.exceptions.HTTPError as errh:
+            print ("Http Error: ",errh)
+        except requests.exceptions.ConnectionError as errc:
+            print ("Error Connecting: ",errc)
+        except requests.exceptions.Timeout as errt:
+            print ("Timeout Error: ",errt)
